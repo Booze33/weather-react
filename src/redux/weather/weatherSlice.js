f236069a0191ae82cc7270853aa8cfc259289455
Booze33 <tebetisloh@gmail.com>
2023-05-26 01:40:27 -0500

    Update Weather Component

--- src/redux/weather/weatherSlice.js
+++ src/redux/weather/weatherSlice.js
@@ -7,10 +7,17 @@ export const fetchWeatherData = createAsyncThunk('fetchWeather', async (location
   const apiURL = `https://api.openweathermap.org/data/2.5/forecast?q=${locationName}&appid=${apiKey}`;
 
   const response = await axios.get(apiURL);
+  console.log(response.data);
 
   const weather = {
+    city: response.data.city.name,
+    img: response.data.list[0].weather[0].icon,
+    main: response.data.list[0].weather[0].main,
     condition: response.data.list[0].weather[0].description,
     temperature: response.data.list[0].main.temp,
+    tomorrow: response.data.list[8].main.temp,
+    nextTomorrow: response.data.list[16].main.temp,
+    nextThreeDays: response.data.list[24].main.temp,
   };
 
   return weather;
